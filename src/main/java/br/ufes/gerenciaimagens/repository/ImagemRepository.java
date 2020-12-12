648c341736084d1c19c1d3190d77194878ecf528
Rômulo Borges <romuloborges111@gmail.com>
2020-12-12 19:23:34 -0300

    Início da Repository

---
+++ src/main/java/br/ufes/gerenciaimagens/repository/ImagemRepository.java
@@ -0,0 +1,16 @@
+package br.ufes.gerenciaimagens.repository;
+
+import br.ufes.gerenciaimagens.model.Imagem;
+import java.util.List;
+
+/**
+ *
+ * @author rborges
+ */
+public class ImagemRepository {
+    
+    public List<Imagem> obterTodasNaoExcluidas() throws Exception {
+        return null;
+    }
+    
+}
