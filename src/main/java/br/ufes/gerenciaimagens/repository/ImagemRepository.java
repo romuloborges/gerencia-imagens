9528994325029f148b1e5ebcc949fdc9d329edf6
Rômulo Borges <romuloborges111@gmail.com>
2020-12-12 20:56:07 -0300

    Adiciona ImagemService e atualiza ImagemRepository para usar método da DAO

--- src/main/java/br/ufes/gerenciaimagens/repository/ImagemRepository.java
+++ src/main/java/br/ufes/gerenciaimagens/repository/ImagemRepository.java
@@ -1,5 +1,7 @@
 package br.ufes.gerenciaimagens.repository;
 
+import br.ufes.gerenciaimagens.dao.interfaces.IImagemDAO;
+import br.ufes.gerenciaimagens.dao.sqlite.impl.ImagemSqliteDAO;
 import br.ufes.gerenciaimagens.model.Imagem;
 import java.util.List;
 
@@ -9,8 +11,14 @@ import java.util.List;
  */
 public class ImagemRepository {
     
+    private IImagemDAO imagemDAO;
+    
+    public ImagemRepository() {
+        imagemDAO = new ImagemSqliteDAO();
+    }
+    
     public List<Imagem> obterTodasNaoExcluidas() throws Exception {
-        return null;
+        return imagemDAO.obterTodasNaoExcluidas();
     }
     
 }
---
+++ src/main/java/br/ufes/gerenciaimagens/service/ImagemService.java
@@ -0,0 +1,20 @@
+package br.ufes.gerenciaimagens.service;
+
+import java.util.List;
+
+import br.ufes.gerenciaimagens.model.Imagem;
+import br.ufes.gerenciaimagens.repository.ImagemRepository;
+
+/**
+ *
+ * @author rborges
+ */
+public class ImagemService {
+    
+    private ImagemRepository imagemRepository;
+
+    public List<Imagem> obterTodasNaoExcluidas() throws Exception {
+        return imagemRepository.obterTodasNaoExcluidas();
+    }
+    
+}
