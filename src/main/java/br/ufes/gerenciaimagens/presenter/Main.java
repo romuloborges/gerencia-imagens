23edd54f0068c4e7e85085a76ce82cd0dfe8486e
Rômulo Borges <romuloborges111@gmail.com>
2020-12-12 20:58:26 -0300

    Adiciona ListamImagemView e sua presenter

---
+++ src/main/java/br/ufes/gerenciaimagens/presenter/Main.java
@@ -0,0 +1,13 @@
+package br.ufes.gerenciaimagens.presenter;
+
+/**
+ *
+ * @author rborges
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        new PrincipalPresenter();
+    }
+    
+}
---
+++ src/main/java/br/ufes/gerenciaimagens/presenter/listaimagem/ListaImagemPresenter.java
@@ -0,0 +1,67 @@
+package br.ufes.gerenciaimagens.presenter.listaimagem;
+
+import br.ufes.gerenciaimagens.model.Imagem;
+import br.ufes.gerenciaimagens.model.interfaces.IImagem;
+import br.ufes.gerenciaimagens.model.proxy.ImagemProxy;
+import br.ufes.gerenciaimagens.presenter.BaseInternalFramePresenter;
+import br.ufes.gerenciaimagens.presenter.listaimagem.renderer.RendererListaImagem;
+import br.ufes.gerenciaimagens.service.ImagemService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.swing.JDesktopPane;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author rborges
+ */
+public class ListaImagemPresenter extends BaseInternalFramePresenter<ListaImagemView> {
+    
+    private ImagemService imagemService;
+    private Map<String, IImagem> mapPathImagem;
+    
+    public ListaImagemPresenter(JDesktopPane desktop) {
+        super(desktop, new ListaImagemView());
+        
+        imagemService = new ImagemService();
+        
+        iniciarListaImagens();
+        
+        getView().setVisible(true);
+    }
+    
+    private void iniciarListaImagens() {
+        try {
+            List<Imagem> imagens = imagemService.obterTodasNaoExcluidas();
+            
+            if (imagens == null) {
+                throw new Exception("Erro ao carregar imagens");
+            }
+            
+            mapPathImagem = new HashMap<>();
+            
+            String[] listData = new String[imagens.size()];
+            
+            for(int i = 0; i < imagens.size(); i++) {
+                Imagem imagem = imagens.get(i);
+                
+                String caminho = imagem.getCaminho();
+                IImagem proxy = new ImagemProxy(imagem);
+                
+                mapPathImagem.put(caminho, proxy);
+                listData[i] = caminho; 
+            }
+            
+            JList list = getView().getListImagem();
+            
+            list.setListData(listData);
+            list.setCellRenderer(new RendererListaImagem(mapPathImagem));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
+}
